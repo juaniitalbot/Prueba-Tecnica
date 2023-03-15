547cec732a6e4fa054c17a5d073498ec4cb057d0
juaniitalbot <110404548+juaniitalbot@users.noreply.github.com>
2023-03-15 12:28:14 -0300

    Delete Ejercicio 6.sql

--- Ejercicio 6.sql
+++
@@ -1,8 +0,0 @@
-SELECT fp1.*
-FROM pronósticos_de_ventas p1
-INNER JOIN (
-  SELECT día, artículo, local, MAX(fecha_pronóstico) AS max_fecha_pronóstico
-  FROM pronósticos_de_ventas
-  WHERE fecha_pronóstico <= día
-  GROUP BY día, artículo, local
-) fp2 ON fp1.día = fp2.día AND fp1.artículo = fp2.artículo AND fp1.local = fp2.local AND fp1.fecha_pronóstico = fp2.max_fecha_pronóstico
